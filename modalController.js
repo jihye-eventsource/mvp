ebe8109b69acb974ab1a47b5ef722d751683228d
chuan-yu <chuan1994@hotmail.com>
2016-04-23 12:14:29 +1200

    added in modal controller and sample question json file

---
+++ modalController.js
@@ -0,0 +1,24 @@
+var Sequences = {};
+var Questions = {};
+var sequencesLoaded = false;
+$(document).ready( function(){
+	
+});
+
+function loadJSON(eventType){
+	console.log(eventType.id);
+
+
+}
+
+function generateModals(){
+
+}
+
+function nextQuestion(){
+
+}
+
+function previousQuestion(){
+
+}
