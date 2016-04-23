c22f0823625959bc07326c73cce70d367181f1dd
chuan-yu <chuan1994@hotmail.com>
2016-04-23 14:09:48 +1200

    added map for questions and their corresponding inner html

--- modalController.js
+++ modalController.js
@@ -1,6 +1,9 @@
-var Sequences = {};
-var Questions = {};
-var sequencesLoaded = false;
+var questions = {};
+var questionCount = 0;
+var currentQuestion = 0;
+var progress = 0;
+var question_Modal_Map{};
+
 $(document).ready( function(){
 	
 });
@@ -10,20 +13,71 @@ function loadJSON(eventType){
 		generateModals(data);
 
 	});
-
-
 }
 
 function generateModals(data){
-	console.log(data);
-	Questions = data.Questions;
-	for (int i = 0; )
+	questionCount = data.TotalCount;
+	questions = data.Questions;
+	questionCount = questions.length;
+	for (var q in questions){
+		if(q.Type == "SingleWText"){
+
+		}else if ( q.Type == "SingleWOTEXT"){
+
+		}else if( q.Type == "Calendar"){
+
+		}else if( q.Type == "Form"){
+
+		}else if( q.Type == ""){
+
+		}else if( q.Type == ""){
+
+		}else if( q.Type == ""){
+			
+		}else if( q.Type == ""){
+			
+		}else if( q.Type == ""){
+			
+		}
+	};
+
+	nextQuestion();
 }
 
 function nextQuestion(){
-
+	
+	if(questionCount>0 && currentQuestion<questionCount){
+		currentQuestion++;
+		setProgress();
+	};
 }
 
 function previousQuestion(){
+	if(questionCount>0 && currentQuestion>0){
+		currentQuestion--;
+		setProgress();
+	};
+}
 
+function setProgress(){
+	progress = currentQuestion/questionCount * 100;	
+}
+
+
+function generateSingleModal(q){
+	var innerHTML;
+	questionModalMap[key(q)] = innerHTML;
+}
+
+function generateCalendarModal(q){
+	var innerHTML;
+	questionModalMap[key(q)] = innerHTML;
+}
+function generateMultiModal(q){
+	var innerHTML;
+	questionModalMap[key(q)] = innerHTML;
+}
+
+var key = function(obj){
+	return 
 }
